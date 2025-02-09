61cf42542bff8b2e8ef0a4108f1bdeb6a4ebda27
Barkly-Brandon <bubba05barrera@gmail.com>
2025-02-09 14:00:37 -0700

    Added multiplication table code

--- MultiplicationTable_BB/MultplicationTable.vb
+++ MultiplicationTable_BB/MultplicationTable.vb
@@ -1,6 +1,26 @@
 ﻿Module MultplicationTable
 
     Sub Main()
+        Dim userInput As Integer
+        Dim result As String
+
+        Console.WriteLine("Please enter a number you want a multiplication table of.")
+        userInput = CInt(Console.ReadLine())
+        Console.WriteLine($"Enjoy your {userInput} X {userInput} Multiplication Table:")
+
+        For i As Integer = 1 To userInput
+
+            For d As Integer = 1 To userInput
+
+                result = CStr(i * d)
+                result = result.PadLeft(4)
+                Console.Write(result)
+
+            Next
+
+            Console.WriteLine()
+
+        Next
 
     End Sub
 
